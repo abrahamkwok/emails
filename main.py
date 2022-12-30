5547e2d2a2643bb04de90e70381dccb27e62225d
abrahamkwok <112060480+abrahamkwok@users.noreply.github.com>
2022-12-30 14:57:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,94 @@
 from keep_alive import keep_alive
 import os
 import smtplib
-import imghdr
 from email.message import EmailMessage
+import requests
 import time
 
 #chores
 def messages(input):
+
+  #quotes
+  quoteOfWeek = "http://api.quotable.io/random"
+  random_quote = requests.get(quoteOfWeek).json()
+  content = random_quote["content"]
+  author = random_quote["author"]
+  quote = content + "\n\n" + "By " + author
+  
   if input == 0:
-    return "Josh: Trash, Enoch: Bathroom, Adit: Floor, Abraham: Dishes"
+    return f"""
+    <html>
+      <body>
+        <p> Josh: Trash, Enoch: Bathroom, Adit: Floor, Abraham: Dishes
+        <p> {quote} </p>
+      </body>
+    </html>
+    """        
   elif input == 1:
-    return "Josh: Dishes, Enoch: Trash, Adit: Bathroom, Abraham: Floor"
+    return f"""
+    <html>
+      <body>
+        <p> Josh: Dishes, Enoch: Trash, Adit: Bathroom, Abraham: Floor
+        <p> {quote} </p>
+      </body>
+    </html>
+    """   
   elif input == 2:
-    return "Josh: Floor, Enoch: Dishes, Adit: Trash, Abraham: Bathroom"
+    return f"""
+    <html>
+      <body>
+        <p> Josh: Floor, Enoch: Dishes, Adit: Trash, Abraham: Bathroom
+        <p> {quote} </p>
+      </body>
+    </html>
+    """    
   else:
-    return "Josh: Bathroom, Enoch: Floor, Adit: Dishes, Abraham: Trash"
-
-#images/pdfs for other stuff
-    #with open('spiderman.jpg', 'rb') as f:
-    #  file_data = f.read()
-    #  file_type = imghdr.what(f.name)
-    #  file_name = f.name
-
-    #msg.add_attachment(file_data, maintype = 'image', subtype = 'file_type', filename = file_name)
-
+    return f"""
+    <html>
+      <body>
+        <p> Josh: Bathroom, Enoch: Floor, Adit: Dishes, Abraham: Trash
+        <p> {quote} </p>
+      </body>
+    </html>
+    """  
 
 #email sending
 def send_mail(input):
+
+  #account
   EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
   EMAIL_PASSWORD = os.getenv("EMAIL_PASS")
-  contacts = ["abrahamkwok628@gmail.com", "joshschang@berkeley.edu", "estseng@berkeley.edu", "aditgupta@berkeley.edu"]
+
+  #contacts
+  email2 = os.getenv("email2")
+  email3 = os.getenv("email3")
+  email4 = os.getenv("email4")
+  
+  #setting up email
+  contacts = [EMAIL_ADDRESS, email2, email3, email4]
   msg = EmailMessage()
-  msg['Subject'] = 'chores'
+  msg['Subject'] = 'test'
   msg['From'] = 'EMAIL_ADDRESS'
+  #msg['To'] = 'abrahamkwok628@gmail.com'
   msg['To'] = ", ".join(contacts)
+
+  #dog pics
+  dog_info = requests.get('https://api.thedogapi.com/v1/images/search').json()[0]
+  dog_image_url = dog_info['url']
+
+  #sending message
   msg.set_content(messages(input))
+  msg.add_alternative(messages(input) + f'<br/><img src="{dog_image_url}" width="300px">', subtype='html')
+
   with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     smtp.send_message(msg)
 
+#starting server
 keep_alive()
 input = 0
 
+#runs function
 while True:
   send_mail(input % 4)
   input += 1
